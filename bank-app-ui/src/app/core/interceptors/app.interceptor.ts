6e46c4c305f85cb273ce80bf0162b8a5555b20cc
Srikanth Reddy A <sreekanthreddy.com@gmail.com>
2022-06-27 14:40:21 +0400

    Add files via upload

---
+++ bank-app-ui/src/app/core/interceptors/app.interceptor.ts
@@ -0,0 +1,26 @@
+import { HTTP_INTERCEPTORS, HttpEvent } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
+
+const TOKEN_HEADER_KEY = 'x-access-token';
+
+@Injectable()
+export class AppInterceptor implements HttpInterceptor {
+  constructor(private token: TokenStorageService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let authReq = req;
+    const token = this.token.getToken();
+    if (token != null) {
+      authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, token) });
+    }
+    return next.handle(authReq);
+  }
+}
+
+export const appInterceptors = [
+  { provide: HTTP_INTERCEPTORS, useClass: AppInterceptor, multi: true }
+];
