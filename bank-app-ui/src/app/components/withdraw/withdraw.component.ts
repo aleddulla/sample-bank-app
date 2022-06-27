0340a63d4bbcde68671e88532d5322f77ac272e1
Srikanth Reddy A <sreekanthreddy.com@gmail.com>
2022-06-27 14:39:03 +0400

    Add files via upload

---
+++ bank-app-ui/src/app/components/withdraw/withdraw.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Account } from 'src/app/models/account.model';
+import { AccountService } from 'src/app/services/account.service';
+
+@Component({
+  selector: 'app-withdraw',
+  templateUrl: './withdraw.component.html',
+  styleUrls: ['./withdraw.component.css']
+})
+export class WithdrawComponent implements OnInit {
+  account: Account = {
+    title: '',
+    wamount: 0,
+  };
+  submitted = false;
+  constructor(private accountService: AccountService,
+  private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  withdraw() {
+    const data = {
+      accountType: 'Savings',
+      description: this.account.wamount
+    };
+
+    this.accountService.withdraw(data)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.submitted = true;
+          this.router.navigate(['bankDetails']);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+}
