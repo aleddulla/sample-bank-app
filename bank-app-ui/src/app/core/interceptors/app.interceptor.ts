5695b504eef6c654f96aecce59c16eec97da9694
Srikanth Reddy A <sreekanthreddy.com@gmail.com>
2022-06-27 18:29:10 +0400

    Add files via upload

--- bank-app-ui/src/app/core/interceptors/app.interceptor.ts
+++ bank-app-ui/src/app/core/interceptors/app.interceptor.ts
@@ -1,26 +1,40 @@
-import { HTTP_INTERCEPTORS, HttpEvent } from '@angular/common/http';
+import { HttpHeaderResponse, HttpResponse, HttpUserEvent, HttpSentEvent, HttpProgressEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import {
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+} from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import {  Observable, throwError } from 'rxjs';
 import { TokenStorageService } from 'src/app/services/token-storage.service';
-
-const TOKEN_HEADER_KEY = 'x-access-token';
 
 @Injectable()
 export class AppInterceptor implements HttpInterceptor {
-  constructor(private token: TokenStorageService) { }
+  constructor(private token: TokenStorageService) {}
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpSentEvent | HttpHeaderResponse | HttpProgressEvent | HttpResponse<any> | HttpUserEvent<any> | any> {
     let authReq = req;
-    const token = this.token.getToken();
+    let token = this.token.getToken();
     if (token != null) {
-      authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, token) });
+      return next.handle(this.addTokenRequest(req, token))
+      .pipe(
+       );
+    } else {
+      return next.handle(authReq);
     }
-    return next.handle(authReq);
+  }
+
+  private addTokenRequest(request: HttpRequest<any>, token: String): HttpRequest<any> {
+    return request.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+        AccessControlAllowMethos: '*',
+        AccessControlAllowOrigin: '*',
+      },
+    });
   }
 }
-
-export const appInterceptors = [
-  { provide: HTTP_INTERCEPTORS, useClass: AppInterceptor, multi: true }
-];
